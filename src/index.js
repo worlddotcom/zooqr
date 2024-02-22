5251750bc4c698f7aeb02d55d532f58c04a4bb2c
nv <nv@11zon.com>
2024-02-22 13:12:15 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -8,7 +8,20 @@ dotenv.config({
 })
 
 
-connectDB();
+connectDB()
+    .then(() => {
+        app.on("ERROR", (error) => {
+            console.log("ERROR: ", error);
+            throw error;
+        })
+
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at port ${process.env.PORT}`);
+        })
+    })
+    .catch((error) => {
+        console.log("MongoDB connection failed: ", error);
+    })
 
 
 
